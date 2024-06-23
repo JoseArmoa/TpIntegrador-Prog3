6e1ea63d6da28d039ae2281d70e199f6a1646cbb
Gabriel Ormeño <gabii47196186@gmail.com>
2024-06-23 11:55:14 -0300

    Buenas en la parte del dao Agregue un acceso a usuario e hice unos métodos para agregar, eliminar y modificar usuarios (deje comentado los procedimientos almacenados) y los métodos en la parte de negocio

---
+++ TPINT_Grupo_Nr_15_PR3/Dao/AccesoUsuario.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Dao
+{
+    public class AccesoUsuario
+    {
+        AccesoDB db = new AccesoDB();
+
+        public bool existeUsuario(Usuarios usu)
+        {
+            string consulta = "SELECT IdUsuario FROM Usuarios WHERE IdUsuario = '" + usu.iDUsuario + "'";
+            return db.existe(consulta);
+        }
+
+        public void ParametrosAgregar(ref SqlCommand sc, ref Usuarios usu)
+        {
+            SqlParameter SqlParametros = new SqlParameter();
+            SqlParametros = sc.Parameters.Add("@IDUSUARIO", SqlDbType.Int);
+            SqlParametros.Value = usu.iDUsuario;
+            SqlParametros = sc.Parameters.Add("@NOMBREUSUARIO", SqlDbType.VarChar);
+            SqlParametros.Value = usu.nombreUsuario;
+            SqlParametros = sc.Parameters.Add("@CONTRASEÑA", SqlDbType.VarChar);
+            SqlParametros.Value = usu.nombreUsuario;
+            SqlParametros = sc.Parameters.Add("@TIPOUSUARIO", SqlDbType.VarChar);
+            SqlParametros.Value = usu.tipousuario;
+        }
+        public int agregarUsuario(ref Usuarios usu)
+        {
+            SqlCommand sc = new SqlCommand();
+            ParametrosAgregar(ref sc, ref usu);
+            return db.EjecutarProcedimientoAlmacenado(ref sc, "spAgregarUsuario");
+
+            /*
+             * Consulta del procedimiento en db
+                CREATE PROCEDURE spAgregarUsuario
+                (
+	                @IDUSUARIO int,
+	                @NOMBREUSUARIO varchar(15),
+                    @CONTRASENIA varchar(15),
+	                @TIPOUSUARIO varchar(15)
+                ) AS
+                INSERT INTO Sucursal(IdUsuario, NombreUsuario, Contrasenia, TipoUsario)
+                SELECT @IDUSUARIO,  @NOMBREUSUARIO, @CONSTRASENIA, @TIPOUSUARIO
+                GO
+             */
+        }
+
+        public void AgregarParametrosEliminar(ref SqlCommand sc, Usuarios usu)
+        {
+            SqlParameter SqlParametros = new SqlParameter();
+            SqlParametros = sc.Parameters.Add("@IDUSUARIO", SqlDbType.Int);
+            SqlParametros.Value = usu.iDUsuario;
+        }
+
+        public int EliminarUsuario(Usuarios usu)
+        {
+            SqlCommand sc = new SqlCommand();
+            AgregarParametrosEliminar(ref sc, usu);
+            return db.EjecutarProcedimientoAlmacenado(ref sc, "spEliminarUsuario");
+
+            //--spEliminarUsuario
+
+            //CREATE PROCEDURE spEliminarUsuario
+            //(
+            //@IDUSUARIO int
+            //)
+            //AS
+            //DELETE FROM Usuarios
+            //WHERE IdUsuario = @IDUSUARIO
+            //RETURN
+        }
+
+        public void AgregarParametrosActualizar(ref SqlCommand sc, Usuarios usu)
+        {
+            SqlParameter sqlParameter = new SqlParameter();
+            sqlParameter = sc.Parameters.Add("@IDUSUARIO", SqlDbType.Int);
+            sqlParameter.Value = usu.iDUsuario;
+            sqlParameter = sc.Parameters.Add("@NOMBRE", SqlDbType.VarChar);
+            sqlParameter.Value = usu.nombreUsuario;
+            sqlParameter = sc.Parameters.Add("@CONTRASEÑA", SqlDbType.VarChar);
+            sqlParameter.Value = usu.contraseña;
+        }
+
+        public int ActualizarUsuario(Usuarios usu)
+        {
+            SqlCommand sc = new SqlCommand();
+            AgregarParametrosActualizar(ref sc, usu);
+            return db.EjecutarProcedimientoAlmacenado(ref sc, "spActualizarUsuario");
+
+            /*
+                CREATE PROCEDURE spActualizarPaciente
+            (
+                  @IDUSUARIO int,
+                  @NOMBREUsuario varchar(15),
+                  @CONTRASEÑA varchar(15),
+            )
+                AS
+                UPDATE Pacientes
+                SET
+                  IdUsuario = @IDUSUARIO,
+                  NombreUsuario = @NOMBREUSUARIO,
+                  Contrasenia = @CONTRASENIA,               
+                WHERE IdUsuario = @IDUSUARIO
+                RETURN
+             */
+        }
+    }
+}
---
+++ TPINT_Grupo_Nr_15_PR3/Negocio/ControladorUsuario.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dao;
+using Entidades;
+using System.Data;
+
+namespace Negocio
+{
+    public class ControladorUsuario
+    {
+        AccesoUsuario accUsuario = new AccesoUsuario();
+
+
+        public bool agregarUsuario(ref Usuarios usu, ref string mensaje)
+        {
+
+            mensaje = null;
+
+            if (accUsuario.existeUsuario(usu))
+            {
+                mensaje = "El Usuario ya existe en la base de datos";
+                return false;
+            }
+            int filasAfectadas = accUsuario.agregarUsuario(ref usu);
+
+
+            if (filasAfectadas > 0)
+            {
+                return true;
+            }
+            mensaje = "Hubo un error al guardar";
+            return false;
+        }
+
+        public bool EliminarUsuario(Usuarios usu)
+        {
+            int filasAfectadas = accUsuario.EliminarUsuario(usu);
+            if (filasAfectadas > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool ActualizarUsuario(Usuarios usu)
+        {
+            int filasAfectadas = accUsuario.ActualizarUsuario(usu);
+            if (filasAfectadas == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
