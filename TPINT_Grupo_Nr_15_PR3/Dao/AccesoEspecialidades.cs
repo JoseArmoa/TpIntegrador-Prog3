e71a673d42e8a9f0ccd7210dca633728cb592ae2
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2024-06-24 00:47:30 -0300

    Creo AccesoEspecialidades en DAO

---
+++ TPINT_Grupo_Nr_15_PR3/Dao/AccesoEspecialidades.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Dao
+{
+    class AccesoEspecialidades
+    {
+        static AccesoDB acceso = new AccesoDB();
+
+        public AccesoEspecialidades() {}
+
+        public DataTable obtenerTabla()
+        {
+            string consulta = "SELECT IdEspecialidad as ID, NombreEspecialidad AS Especialidad  FROM Especialidades";
+
+            return acceso.ObtenerTabla("Especialidades", consulta);
+        }
+    }
+}
